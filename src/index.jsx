212ea81b15e28275714da5d7d541707a3a6541e0
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-02-06 18:54:32 -0500

    finished lesson

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import {createRoot} from 'react-dom';
+import "./index.scss";
+
+const MyFlixApplication = () => {
+    return (
+      <div className="my-flix">
+        <div>Good morning</div>
+      </div>
+    );
+  };
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+
+root.render(<myFlixApplication/>);
