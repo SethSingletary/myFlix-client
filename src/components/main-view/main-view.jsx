2d2fd3156f1a9531ba9f256c5176172c2b72f099
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-02-13 13:36:32 -0500

    added login

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
 import PropTypes from 'prop-types';
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, users] = useState(null);
+
+
     useEffect(() => {
         fetch("https://my-flix2.herokuapp.com/movies")
           .then((response) => response.json())
@@ -24,6 +27,10 @@ export const MainView = () => {
             setMovies(moviesFromApi);
           });
       }, []);
+
+    if(!user){
+      return <LoginView onLoggedIn={(user) => setUser(user)}/>
+    }
 
 
     if(selectedMovie){
