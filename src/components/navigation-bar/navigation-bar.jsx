d27ce3ec49a6b8ab98d60de5406838a3f3caaebb
SethSingletary <122328286+SethSingletary@users.noreply.github.com>
2023-04-21 08:44:52 -0400

    Update navigation-bar.jsx

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,14 +1,14 @@
 import { Button, Navbar } from "react-bootstrap";
 import { ProfileView } from "../profile-view/profile-view";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 export const NavigationBar = () => {
 
     return(
         
             <ul>
-                <li><a href="https://clever-pixie-e8d67c.netlify.app/">Home</a></li>
-                <li><a href="https://clever-pixie-e8d67c.netlify.app/profile">Profile</a></li>
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/profile'}>Profile</Link></li>
                 <li><Button onClick={handleLogout()}>Logout</Button></li>
             </ul>
         
